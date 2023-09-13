cd0f5a085d4369ca6b5b0ebd2e478cea79bb5a02
kmobl <moblkourosh@gmail.com>
2023-09-13 12:39:57 -0500

    fixing outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,8 @@ output "tstate_bucket_name" {
   value = module.s3-tstate.id
   description = "The name of the terraform state bucket"
 }
+
+output "s3_key_iam" {
+  value = data.aws_iam_policy_document.s3_key.json
+  description = "The name of the terraform state bucket"
+}
