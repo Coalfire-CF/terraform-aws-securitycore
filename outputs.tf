0dec0639d0b5afa4ac09ff5d37f0126d478e7459
kmobl <moblkourosh@gmail.com>
2023-09-26 13:36:25 -0500

    update module link

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "s3_key_arn" {
-  value = module.s3_kms_key.kms_key_arn
+  value = module.s3_kms_key.*.kms_key_arn
   description = "The arn of the s3 kms key"
 }
 
 output "s3_key_id" {
-  value = module.s3_kms_key.kms_key_id
+  value = module.s3_kms_key.*.kms_key_id
   description = "The id of the s3 key"
 }
 
 output "dynamo_key_arn" {
-  value = module.dynamo_kms_key.kms_key_arn
+  value = module.dynamo_kms_key.*.kms_key_arn
   description = "The arn of the dynamo kms key"
 }
 
 output "dynamo_key_id" {
-  value = module.dynamo_kms_key.kms_key_id
+  value = module.dynamo_kms_key.*.kms_key_id
   description = "The id of the dynamo key"
 }
 
