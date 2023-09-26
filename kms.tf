3b562c29a25e3a96c50398d49bfc00e66aa22212
kmobl <moblkourosh@gmail.com>
2023-09-26 14:12:56 -0500

    fixed vars type

--- kms.tf
+++ kms.tf
@@ -1,5 +1,4 @@
 module "s3_kms_key" {
-  count = var.create_s3_kms_key ? 1 : 0
   source = "github.com/Coalfire-CF/terraform-aws-kms"
 
   key_policy            = data.aws_iam_policy_document.s3_key.json
@@ -8,8 +7,6 @@ module "s3_kms_key" {
 }
 
 module "dynamo_kms_key" {
-  count = var.create_dynamo_kms_key ? 1 : 0
-
   source = "github.com/Coalfire-CF/terraform-aws-kms"
 
   kms_key_resource_type = "dyanmo"
